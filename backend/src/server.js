5ff48c726f80ba7e9be7c70c5749f6a7e7f91ca5
NguyenMinh03 <minhnguyenphuc121103@gmail.com>
2026-07-25 23:52:43 +0300

    feat: implement MongoDB connection and refactor server initialization

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './libs/db.js';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(express.json());
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
