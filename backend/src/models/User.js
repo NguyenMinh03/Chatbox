5ec6db3eff31919a08ddf9a9f26532725fb95206
NguyenMinh03 <minhnguyenphuc121103@gmail.com>
2026-07-26 13:46:43 +0300

    feat: add User model with schema definition for user data

---
+++ backend/src/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    hashedPassword: {
+        type: String,
+        required: true,
+    },
+    displayName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    avatarUrl: {
+        type: String
+    },
+    avatarId: {
+        type: String
+    },
+    bio : {
+        type: String,
+        maxlength: 500,
+    },
+    phone: {
+        type: String,
+        sparse: true,
+},
+}, { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+export default User;
