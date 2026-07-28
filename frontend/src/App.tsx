4de8b322b539c71881acd80e16c3979f3c6abce8
NguyenMinh03 <minhnguyenphuc121103@gmail.com>
2026-07-28 22:30:22 +0300

    feat: add signup form and related UI components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
-
+import { BrowserRouter, Route, Routes} from 'react-router';
+import SignUpPage from './pages/SignUpPage';
+import SignInPage from './pages/SignInPage';
+import ChatAppPage from './pages/ChatAppPage'
+import { Toaster } from 'sonner';
 function App() {
   return (
     <>
-      
+      <Toaster richColors/>
+      <BrowserRouter>
+        <Routes>
+        {/* public routes here */}
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          {/* Protected routes here */}
+          {/* create Protected routes here */}
+          <Route path="/" element={<ChatAppPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/ChatAppPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatAppPage = () => {
+  return (
+    <div>
+      chat app page
+    </div>
+  )
+}
+
+export default ChatAppPage
