c413e67bd7d3c8ab24a930fb63218b21bfae502f
NguyenMinh03 <minhnguyenphuc121103@gmail.com>
2026-08-05 21:42:55 +0300

    feat: add UI components for collapsible, dropdown menu, sheet, sidebar, skeleton, and tooltip

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,4 +1,4 @@
-import Logout from '@/components/auth/logout'
+import Logout from '@/components/auth/Logout'
 import { useAuthStore } from '@/stores/useAuthStore'
 import { Button } from '@base-ui/react/button';
 import React from 'react'
