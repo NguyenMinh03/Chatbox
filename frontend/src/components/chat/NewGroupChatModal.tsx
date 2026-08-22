19e6e44509a13cd06359f32e1f1cc2c900ed505e
NguyenMinh03 <minhnguyenphuc121103@gmail.com>
2026-08-22 14:30:27 +0300

    feat: implement NewGroupChatModal; add friend selection and group creation functionality with invite suggestions and selected users list

--- frontend/src/components/chat/NewGroupChatModal.tsx
+++ frontend/src/components/chat/NewGroupChatModal.tsx
@@ -1,10 +1,155 @@
-import React from 'react'
-
+import { useFriendStore } from "@/stores/useFriendStore";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";import { useState } from "react"
+import type { Friend } from "@/types/user";
+import { Button } from "../ui/button";
+import { UserPlus, Users } from "lucide-react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { toast } from "sonner";
+import { useChatStore } from "@/stores/useChatStore";
+import IniviteSuggestionList from "../newGroupChat/IniviteSuggestionList";
+import SelectedUsersList from "../newGroupChat/SelectedUsersList";
 const NewGroupChatModal = () => {
+  const [groupName, setGroupName ] = useState("");
+  const [search ,setSearch] = useState("");
+  const {friends, getFriends} = useFriendStore();
+  const [invitedUsers, setInvitedUsers] = useState<Friend[]>([]);
+  const { loading, createConversation } = useChatStore();
+
+  const handleGetFriends = async () => {
+    await getFriends();
+  }
+  const handleSelectFriend = (friend: Friend) => {
+    setInvitedUsers([...invitedUsers, friend]);
+    setSearch("");
+  };
+  const handleRemoveFriend = (friend: Friend) => {
+    setInvitedUsers(invitedUsers.filter((u) => u._id !== friend._id));
+  };
+  const handleSubmit = async (e: React.FormEvent) => {
+    try {
+      e.preventDefault();
+      if (invitedUsers.length === 0) {
+        toast.warning("At least 1 member invited");
+        return;
+      }
+
+      await createConversation(
+        "group",
+        groupName,
+        invitedUsers.map((u) => u._id)
+      );
+
+      setSearch("");
+      setInvitedUsers([]);
+    } catch (error) {
+      console.error("Fail in handle submit in NewGroupChatModal:", error);
+    }
+  };
+
+  const filteredFriends = friends.filter(
+    (friend) =>
+      friend.displayName.toLowerCase().includes(search.toLowerCase()) &&
+      !invitedUsers.some((u) => u._id === friend._id)
+  );
   return (
-    <div>
-      
-    </div>
+    <Dialog>
+      <DialogTrigger
+        onClick={handleGetFriends}
+        render={
+          <Button
+            variant="ghost"
+            className="flex z-10 justify-center items-center size-5 rounded-full hover:bg-sidebar-accent transition cursor-pointer"
+          />
+        }
+      >
+        <Users className="size-4" />
+        <span className="sr-only">Create Group</span>
+      </DialogTrigger>
+       <DialogContent className="sm:max-w-[425px] border-none">
+        <DialogHeader>
+          <DialogTitle className="capitalize">Create new group chat</DialogTitle>
+        </DialogHeader>
+         <form
+          className="space-y-4"
+          onSubmit={handleSubmit}
+        >
+         {/* Group Name */}
+          <div className="space-y-2">
+            <Label
+              htmlFor="groupName"
+              className="text-sm font-semibold"
+            >
+              Group Name
+            </Label>
+            <Input
+              id="groupName"
+              placeholder="Type Group Name here"
+              className="glass border-border/50 focus:border-primary/50 transition-smooth"
+              value={groupName}
+              onChange={(e) => setGroupName(e.target.value)}
+              required
+            />
+          </div>
+           {/* Invite member */}
+          <div className="space-y-2">
+            <Label
+              htmlFor="invite"
+              className="text-sm font-semibold"
+            >
+              Invite member
+            </Label>
+
+            <Input
+              id="invite"
+              placeholder="Find user..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="flex-1"
+            />
+
+            {/* Recommendation list  */}
+            {search && filteredFriends.length > 0 && (
+              <IniviteSuggestionList
+                filteredFriends={filteredFriends}
+                onSelect={handleSelectFriend}
+              />
+            )}
+
+            {/* Choose user list */}
+            <SelectedUsersList
+              invitedUsers={invitedUsers}
+              onRemove={handleRemoveFriend}
+            />
+          </div>
+          <DialogFooter>
+            <Button
+              type="submit"
+              disabled={loading}
+              className="flex-1 bg-gradient-chat text-white hover:opacity-90 transition-smooth"
+            >
+              {loading ? (
+                <span>Creating...</span>
+              ) : (
+                <>
+                  <UserPlus className="size-4 mr-2" />
+                  Create Group
+                </>
+              )}
+            </Button>
+          </DialogFooter>
+        </form>
+
+
+       </DialogContent>
+    </Dialog>
   )
 }
 
